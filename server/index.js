20b9a08999edd951aef719db565e6f4801f4187f
doctafaustus <clowd150@gmail.com>
2026-04-01 00:43:57 -0500

    Serve dist folder on Render

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { connectDB } from './db.js'
 import usersRouter from './routes/users.js'
 import lessonsRouter from './routes/lessons.js'
@@ -10,6 +12,7 @@ import thoughtsRouter from './routes/thoughts.js'
 
 const app = express()
 const PORT = process.env.PORT || 5000
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 app.use(cors())
 app.use(express.json())
@@ -21,6 +24,12 @@ app.use('/api/reflections', reflectionsRouter)
 app.use('/api/thoughts', thoughtsRouter)
 
 app.get('/api/health', (req, res) => res.json({ status: 'ok' }))
+
+// Serve React app
+app.use(express.static(path.join(__dirname, '../dist')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'))
+})
 
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
